b16aa0dae670ec331e83819298e460de28f3259a
PHBelfort <phbelfort@gmail.com>
2017-04-06 09:19:48 -0400

    Projeto funcionando

--- app_veiculos/views.py
+++ app_veiculos/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from app_veiculos.models import Veiculo
+
 
 def index(request):
-    return render(request, 'index.html')
+
+    consulta_ao_banco = Veiculo.objects.all()
+
+    return render(request, 'index.html',
+                  {'consulta_ao_banco': consulta_ao_banco})
